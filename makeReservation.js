be240cf055cfb391aa08749dbc0c29bedf74e048
Brian K <karabinchak.brian@gmail.com>
2017-06-24 11:13:32 -0400

    Added dummy data

--- makeReservation.js
+++ makeReservation.js
@@ -18,8 +18,55 @@ app.use(bodyParser.json({
   type: "application/vnd.api+json"
 }));
 
-var currentWaitingList = [];
-var extendedWaitingList = [];
+var currentWaitingList = [
+	{
+		customerName: 'Brian',
+		phoneNumber: '9085106980',
+		customerEmail: 'fake@aol.com',
+		customerID: '12'
+	},
+	{
+		customerName: 'Elana',
+		phoneNumber: '1233213333',
+		customerEmail: '123@yahoo.com',
+		customerID: '11'
+	},
+	{
+		customerName: 'Rebecca',
+		phoneNumber: '5555555555',
+		customerEmail: 'blahblah@hotmail.com',
+		customerID: '10'
+	},
+	{
+		customerName: 'Ariene',
+		phoneNumber: '11111111111',
+		customerEmail: 'blergh@altavista.com',
+		customerID: '9'
+	}
+	{
+		customerName: 'Customer 5',
+		phoneNumber: '55555555555',
+		customerEmail: 'abcd@askjeeves.com',
+		customerID: '8'
+	}
+	];
+	
+var extendedWaitingList = [
+	{
+		customerName: 'Extra Waiting 1',
+		phoneNumber: '8888888888',
+		customerEmail: 'something@somethingelse.com',
+		customerID: '13'
+	},
+	{
+		customerName: 'Extra Waiting 2',
+		phoneNumber: '9999999999',
+		customerEmail: 'nothing@something.com',
+		customerID: '15'
+	},
+];
+
+
 
 //Reserve a New Table
 app.post("/api/new", function(req, res) {
@@ -32,4 +79,9 @@ app.post("/api/new", function(req, res) {
 	} else {
 		currentWaitingList.push(newReservation);
 	}
+
+});
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
 });
